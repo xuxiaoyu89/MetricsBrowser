316c752c2fee2453a5434cb9a3fddbce58e6e835
Xiaoyu Xu <xiaoyu.xu@mongodb.com>
2023-03-10 12:29:19 -0500

    update inputs

--- metrics-browser/src/SeriesControlPanel.js
+++ metrics-browser/src/SeriesControlPanel.js
@@ -7,14 +7,14 @@ class SeriesControlPanel extends Component {
         for (const name in this.props.metrics) {
             if (this.props.metrics[name]) {
                 metricNameList.push(<div>
-                    <button class= {'button ' + (this.props.metrics[name] ? 'selectedButton' : 'unSelectedButton')} onClick={this.props.toggleMetricName.bind(this, name)}>{name}</button>
+                    <button className= {'button ' + (this.props.metrics[name] ? 'selectedButton' : 'unSelectedButton')} onClick={this.props.toggleMetricName.bind(this, name)}>{name}</button>
                 </div>);
             }
         }
         for (const name in this.props.metrics) {
             if (!this.props.metrics[name]) {
                 metricNameList.push(<div>
-                    <button class={'button ' + (this.props.metrics[name] ? 'selectedButton' : 'unSelectedButton')} onClick={this.props.toggleMetricName.bind(this, name)}>{name}</button>
+                    <button className={'button ' + (this.props.metrics[name] ? 'selectedButton' : 'unSelectedButton')} onClick={this.props.toggleMetricName.bind(this, name)}>{name}</button>
                 </div>);
             }
         }
--- metrics-browser/src/SeriesView.js
+++ metrics-browser/src/SeriesView.js
@@ -12,20 +12,32 @@ class SeriesView extends Component {
         // this.apiPrivateKey = "85824e2e-02f0-42c8-aa67-079879e9edb8";
         // this.groupID = "61829a294c0dd40aeae25af1";
         // this.appID = "636276a6dab3c68df06a73ad";
-        this.apiPublicKey = "bjwqwyko";
-        this.apiPrivateKey = "36d0ac8b-df27-49e1-8498-fcfd451660ba";
-        this.groupID = "63ff7f8397f5ea615df7db2e";
-        this.appID = "636276a6dab3c68df06a73ad";
-        this.hostID = "";
+
+        // this.apiPublicKey = "bjwqwyko";
+        // this.apiPrivateKey = "36d0ac8b-df27-49e1-8498-fcfd451660ba";
+        // this.groupID = "63ff7f8397f5ea615df7db2e";
+        // this.appID = "636276a6dab3c68df06a73ad";
+        // this.hostID = "";
         this.showMetricNames = this.showMetricNames.bind(this);
         this.showMetrics = this.showMetrics.bind(this);
         this.metricNameHandler = this.metricNameHandler.bind(this);
         this.toggleMetricName = this.toggleMetricName.bind(this);
+        this.handleGroupIDChange = this.handleGroupIDChange.bind(this);
+        this.handleAppIDChange = this.handleAppIDChange.bind(this);
+        this.handleHostIDChange = this.handleHostIDChange.bind(this);
+        this.handlePublicKeyChange = this.handlePublicKeyChange.bind(this);
+        this.handlePrivateKeyChange = this.handlePrivateKeyChange.bind(this);
+
         this.showMetricsStyle = {
             display: "none"
         };
 
         this.state = {
+            groupID: "63ff7f8397f5ea615df7db2e",
+            appID: "636276a6dab3c68df06a73ad",
+            hostID: "",
+            publicKey: "bjwqwyko",
+            privateKey: "36d0ac8b-df27-49e1-8498-fcfd451660ba",
             metrics: {} // {metricName: checked}
         };
     }
@@ -49,24 +61,50 @@ class SeriesView extends Component {
     }
     // curl --user "bjwqwyko:36d0ac8b-df27-49e1-8498-fcfd451660ba" --digest --header "Content-Type: application/json" --include --request GET "http://localhost:8080/api/atlas/v1.0/groups/63ff7f8397f5ea615df7db2e/application/636276a6dab3c68df06a73ad/realm/metrics"
     // curl --user "bjwqwyko:36d0ac8b-df27-49e1-8498-fcfd451660ba" --digest --header "Content-Type: application/json" --include --request GET "http://localhost:8080/api/atlas/v1.0/groups/63ff7f8397f5ea615df7db2e/application/636276a6dab3c68df06a73ad/realm/measurements?granularity=PT1M&period=PT1H"
+
+
+    handleGroupIDChange(e) {
+        this.setState({ groupID: e.target.value });
+    }
+
+    handleAppIDChange(e) {
+        this.setState({ appID: e.target.value });
+    }
+
+    handleHostIDChange(e) {
+        this.setState({ hostID: e.target.value });
+    }
+
+    handlePublicKeyChange(e) {
+        this.setState({ publicKey: e.target.value });
+    }
+
+    handlePrivateKeyChange(e) {
+        this.setState({ privateKey: e.target.value });
+    }
+
     render() {
         return <>
-            <label>Public API: </label>
-            <input type="text" id="publicAPI" name="publicAPI"></input>
+            <label>GroupID: </label>
+            <input type="text" value={this.state.groupID} onChange={this.handleGroupIDChange}></input><br/>
+            <label>HostID: </label>
+            <input type="text" value={this.state.hostID} onChange={this.handleHostIDChange}></input><br/>
+            <label>AppID (for Realm): </label>
+            <input type="text" value={this.state.appID} onChange={this.handleAppIDChange}></input><br/>
             <label>Public Key: </label>
-            <input type="text" id="publicKey" name="publicKey"></input>
+            <input type="text" value={this.state.publicKey} onChange={this.handlePublicKeyChange}></input><br/>
             <label>Private Key: </label>
-            <input type="text" id="privateKey" name="privateKey"></input>
-            <div class="button-container">
-                <button class="button1" onClick={this.showMetricNames}>Show Avaiable Metrics</button>
+            <input type="text" value={this.state.privateKey} onChange={this.handlePrivateKeyChange}></input><br/>
+            <div className="button-container">
+                <button className="button1" onClick={this.showMetricNames}>Show Avaiable Metrics</button>
             </div>
             <div id="seriesView">
                 <div id="seriesSelector" className="seriesSelector">
                     <SeriesControlPanel metrics={this.state.metrics} toggleMetricName={this.toggleMetricName} />
                 </div>
             </div>
-            <div class="button-container">
-                <button class="button1" id="showMetrics" onClick={this.showMetrics} style={this.showMetricsStyle}>Display Selected Metrics</button>
+            <div className="button-container">
+                <button className="button1" id="showMetrics" onClick={this.showMetrics} style={this.showMetricsStyle}>Display Selected Metrics</button>
             </div>
             <div id="chartsContainer" className="chartsContainer"></div>
         </>;
@@ -103,15 +141,15 @@ class SeriesView extends Component {
     }
 
     showMetricNames() {
-        const metricsUrl = this.getBaasMetricsPublicUrl(this.groupID, this.appID);
+        const metricsUrl = this.getBaasMetricsPublicUrl(this.state.groupID, this.state.appID);
 
         const requestOptions = {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
                 url: metricsUrl,
-                publicKey: this.apiPublicKey,
-                privateKey: this.apiPrivateKey
+                publicKey: this.state.publicKey,
+                privateKey: this.state.privateKey
             })
         };
         fetch('/api', requestOptions)
@@ -121,15 +159,15 @@ class SeriesView extends Component {
     }
 
     showMetrics() {
-        const measurementsUrl = this.getBaasMeasurementsPublicUrl(this.groupID, this.appID);
+        const measurementsUrl = this.getBaasMeasurementsPublicUrl(this.state.groupID, this.state.appID);
 
         const requestOptions = {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
                 url: measurementsUrl,
-                publicKey: this.apiPublicKey,
-                privateKey: this.apiPrivateKey
+                publicKey: this.state.publicKey,
+                privateKey: this.state.privateKey
             })
         };
         fetch('/api', requestOptions)
